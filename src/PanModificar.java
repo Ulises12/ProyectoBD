7f824567e3a14d4a15cdb508102ac6a1efa3f864
Arturo Alonso <thefang12@gmail.com>
2016-11-21 22:04:27 -0600

    meh

--- src/PanModificar.java
+++ src/PanModificar.java
@@ -8,12 +8,31 @@
  *
  * @author USER1
  */
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.Mongo;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class PanModificar extends javax.swing.JFrame {
 
     /**
      * Creates new form PanModificar
      */
+    DB db;
+    DBCollection tabla;
     public PanModificar() {
+          //Conexion a mongoDb
+        try {
+            Mongo mongo = new Mongo("localhost", 27017 );
+            db= mongo.getDB("Escuela"); 
+            tabla= db.getCollection("Alumnos");
+            
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(Inicio.class.getName()).log(Level.SEVERE, null, ex);
+        }
         initComponents();
     }
 
